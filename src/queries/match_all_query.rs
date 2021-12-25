c58321534618826fdf8ea4afdf9a1a41f2757608
filipesilva <filipe.silva@onlineapp.com.br>
2021-12-25 03:12:09 -0300

    refactor: removing boost from match_all_query

--- src/queries/match_all_query.rs
+++ src/queries/match_all_query.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 use serde::Serialize;
 use serde::Deserialize;
 use serde_json::json;
@@ -7,30 +5,16 @@ use serde_json::json;
 use super::Query;
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct MatchAllQuery {
-    body: HashMap<String, String>
-}
-
-impl MatchAllQuery {
-    pub fn new() -> MatchAllQuery {
-        MatchAllQuery {
-            body: HashMap::new()
-        }
-    }
-}
+pub struct MatchAllQuery;
 
 impl Query for MatchAllQuery {
     fn get_type(&self) -> &str {
         "match_all"
     }
 
-    fn set_boost(&mut self, boost: i32) -> () {
-        self.body.insert("boost".to_string(), boost.to_string());
-    }
-    
     fn to_json(&self) -> String {
         let json = json!({
-            self.get_type(): self.body
+            self.get_type(): {}
         });
 
         serde_json::to_string(&json).unwrap()
@@ -43,33 +27,15 @@ mod tests {
 
     #[test]
     fn should_return_match_all_from_type() {
-        let query = MatchAllQuery::new();
+        let query = MatchAllQuery;
 
         assert_eq!(query.get_type(), "match_all")
     }
 
     #[test]
-    fn should_set_boost_in_body() {
-        let mut query = MatchAllQuery::new();
-
-        query.set_boost(12);
-
-        assert_eq!(query.body.get("boost").unwrap(), "12");
-    }
-
-    #[test]
     fn should_retrieve_valid_json() {
-        let query = MatchAllQuery::new();
+        let query = MatchAllQuery;
 
         assert_eq!(query.to_json(), "{\"match_all\":{}}");
-    }
-
-    #[test]
-    fn should_retrieve_valid_json_with_boost() {
-        let mut query = MatchAllQuery::new();
-
-        query.set_boost(12);
-
-        assert_eq!(query.to_json(), "{\"match_all\":{\"boost\":\"12\"}}")
     }
 }
