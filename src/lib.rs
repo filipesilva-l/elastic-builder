410fa2c254485aac8d7457740a051ac59c9c27d1
filipesilva <filipe.silva@onlineapp.com.br>
2021-12-26 03:28:39 -0300

    feat: adding RequestBody and it's implementation

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,39 @@ mod queries;
 
 use queries::Query;
 
+use serde_json::json;
+
 struct RequestBody<T: Query> {
-    query: T
+    query: T,
 }
 
-impl<T> RequestBody<T> where T: Query {
+impl<T> RequestBody<T>
+where
+    T: Query,
+{
     fn from_query(query: T) -> RequestBody<T> {
-        RequestBody {
-            query
-        }
+        RequestBody { query }
+    }
+
+    fn to_json(self) -> Result<String, serde_json::Error> {
+        let value = json!({ "query": self.query.to_json() });
+
+        serde_json::to_string(&value)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::queries::match_all_query::MatchAllQuery;
+    use super::*;
+
+    #[test]
+    fn should_retrieve_valid_json() {
+        let request_body = RequestBody::from_query(MatchAllQuery::new());
+
+        assert_eq!(
+            request_body.to_json().unwrap(),
+            "{\"query\":{\"match_all\":{}}}"
+        );
     }
 }
