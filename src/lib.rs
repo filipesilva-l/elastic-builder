ddb29e9acf764a0266caf59256b23053c7584bc0
filipesilva <filipe.silva@onlineapp.com.br>
2021-12-25 23:49:52 -0300

    feat: adding RequestBody and it's implementations

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,15 @@
 mod queries;
+
+use queries::Query;
+
+struct RequestBody<T: Query> {
+    query: T
+}
+
+impl<T> RequestBody<T> where T: Query {
+    fn from_query(query: T) -> RequestBody<T> {
+        RequestBody {
+            query
+        }
+    }
+}
