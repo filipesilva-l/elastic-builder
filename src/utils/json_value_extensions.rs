a1041237870e568ec81761a1accbccbcbd8eba7a
filipesilva <filipe.silva@onlineapp.com.br>
2021-12-28 23:13:43 -0300

    refactor: removing unnecessary function from utils

--- src/utils/json_value_extensions.rs
+++ src/utils/json_value_extensions.rs
@@ -7,17 +7,8 @@ pub trait ValueExt {
 
 impl ValueExt for Value {
     fn add_if_it_was_set<T: Serialize>(&mut self, field_name: &str, value_option: &Option<T>) {
-        add_if_it_was_set(self, field_name, value_option, |value| json!(value))
-    }
-}
-
-fn add_if_it_was_set<T>(
-    json_value: &mut Value,
-    field_name: &str,
-    value_option: &Option<T>,
-    get_value: fn(&T) -> Value,
-) {
-    if let Some(value) = value_option {
-        json_value[field_name] = get_value(value);
+        if let Some(value) = value_option {
+            self[field_name] = json!(value);
+        }
     }
 }
